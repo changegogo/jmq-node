a8684e3748e72c8059c54a974ee66dd2299e5f9a
dailliwang <758017188@qq.com>
2018-11-13 10:45:12 +0800

    连接参数可配置

--- test/consumer.test.js
+++ test/consumer.test.js
@@ -3,17 +3,31 @@ const JmqClient = require('..');
 const conf = require('../conf');
 const _ = require('lodash');
 
-describe('consumer测试相等', function() {
-    it('相等', async () => {
-      assert(1 === 1)
-    })
-})
-
 describe('测试consumer拉取消息', function() {
+    let params = {
+        app: 'ddd1234',
+        topic: 'nodetest',
+        user: 'ddd1234',
+        password: '0F75C8EB',
+        host: '192.168.166.40',
+        port: '9090'
+    };
     it('consumer成功拉取消息', async () => {
-        let consumer = new JmqClient().getConsumer();
+        let consumer = new JmqClient(params).getConsumer();
         let info = await consumer.consume();
         assert(info.code === 0);
     })
+    it('consumer成功拉取消息,并且成功消费', async () => {
+        let consumer = new JmqClient(params).getConsumer();
+        let info = await consumer.consume();
+        let ackRes = await consumer.ack(info.result);
+        assert(info.code === 0 && ackRes.code === 0);
+    })
+    // it('consumer成功拉取消息,并且进行重试', async () => {
+    //     let consumer = new JmqClient(params).getConsumer();
+    //     let info = await consumer.consume();
+    //     let retryAckRed = await consumer.retry(info.result, 'err');
+    //     assert(info.code === 0 && retryAckRed.code === 0);
+    // })
 })
 
--- test/producer.test.js
+++ test/producer.test.js
@@ -2,13 +2,21 @@ const assert = require('assert');
 const JmqClient = require('..');
 
 describe('测试producer发送信息', function() {
+      let params = {
+        app: 'ddd123',
+        topic: 'nodetest',
+        user: 'ddd123',
+        password: '9D04192E',
+        host: '192.168.166.40',
+        port: '9090'
+    };
     it('producer成功发送信息到Jmq', async () => {
-      let jmq = new JmqClient();
+      let jmq = new JmqClient(params);
       let result = await jmq.getProducer().produce(['发送消息1']);
       assert(result.code === 0);
     })
     it('消息messages必须为数组,现在是非数组', async () => {
-      let jmq = new JmqClient();
+      let jmq = new JmqClient(params);
       let result = await jmq.getProducer().produce('发送消息1');
       assert(result.code == -1);
     });
